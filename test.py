84f22f111e6f8576c2219f658019b0dd40d94ef3
Pinyoo Thotaboot <pinyoothotaboot@Pinyoos-MacBook-Pro.local>
2018-09-23 21:19:09 +0700

    Update version

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from num_thai.thainumbers import NumThai
 num = NumThai()
 
 n = num.TextThaiToNumber("ลบหนึ่งแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ดล้านหนึ่งแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบเอ็ดจุดหนึ่งสองสามสี่")
-
 print(n)
 
 
